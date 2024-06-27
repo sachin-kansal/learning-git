2c2dbca5e7c48a4770b4fe766429f4504eac12e4
Sachin Kansal <sachin.kansal456@gmail.com>
2024-06-28 00:20:37 +0530

    test1

---
+++ file-editor.py
@@ -0,0 +1,23 @@
+import os
+import fileinput
+import yaml
+import sys
+import pandas as pd
+
+def generic_files_rename(tenantid,i):
+    dest="folder/hellomot-"+ i +".yaml"
+    newname="folder/"+ tenantid + "-"+i +".yaml"
+    try:
+        os.rename(dest,newname) # changing file name
+        file = fileinput.FileInput(newname,inplace=True) # 
+        for line in file:
+            a=line.replace("brajesh",tenantid)
+            print(a,end='')
+        fileinput.close()
+    except FileNotFoundError:
+        print ("files are not present", dest)
+    except FileExistsError:
+        print ("file already exists there",dest,sep=":")
+tenant=sys.argv
+for i in ['con','rep','top','rbac']:
+    generic_files_rename(tenant[1],i)
