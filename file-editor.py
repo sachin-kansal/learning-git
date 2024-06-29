924a007fa69859ba985ff84a1ce5dc96ef4c3f7f
Sachin Kansal <sachin.kansal456@gmail.com>
2024-06-29 16:32:48 +0530

    repo-fix

--- file-editor.py
+++ file-editor.py
@@ -6,19 +6,21 @@ import pandas as pd
 
 def generic_files_rename(tenantid,i):
     print(os.getcwd())
-    dest = os.path.join('.','folder',f"sachin-{i}.yaml")
+    dest = os.path.join('.','folder',f"tenant1-{i}.yaml")
     newname=os.path.join('.','folder', tenantid + '-' + i + '.yaml')
     try:
         os.rename(dest,newname) # changing file name
         file = fileinput.FileInput(newname,inplace=True) # 
         for line in file:
-            a=line.replace("sachin",tenantid)
+            a=line.replace("tenant1",tenantid)
             print(a,end='')
         fileinput.close()
     except FileNotFoundError:
         print ("files are not present", dest)
     except FileExistsError:
         print ("file already exists there",dest,sep=":")
-tenant=sys.argv
+inputs=sys.argv
+tenantid = inputs[1].split("/")[1]
+print(tenantid)
 for i in ['con','rep','top','rbac']:
-    generic_files_rename(tenant[1],i)
+    generic_files_rename(tenantid,i)
